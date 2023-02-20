eacbe980536d196ee3607c350e313aa42c90379e
Ben <benawhiting@gmail.com>
2023-02-19 16:15:57 -0800

    Allow user to set username

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
-// Make the functions for determining winner and adding points
-// typed
-
 let username: string;
+let userScore = 0;
+let comScore = 0;
 
-const btns = document.querySelectorAll('.btn');
+const btns = document.querySelectorAll(".btn");
+const userInput = document.querySelector(".username") as HTMLInputElement;
+const userBtn = document.querySelector(".submit");
+const userPrompt = document.querySelector(".form-group");
+const error = document.querySelector(".error");
+const game = document.querySelector(".game");
 
 btns.forEach((btn) => {
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     console.log(btn.textContent);
+    console.log(username);
   });
+});
+
+userBtn?.addEventListener("click", () => {
+  if (userInput?.value) {
+    username = userInput.value;
+    userPrompt?.classList.add("invisible");
+    game?.classList.add("visible")
+  } else {
+    error!.textContent = "You must enter a username to continue."
+  }
 });
