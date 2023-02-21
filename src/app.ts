3c4629a8a98c9c1f5dc0ab05db810683eca73169
benzelcurry <benawhiting@gmail.com>
2023-02-21 13:36:20 -0800

    Make function for playing rounds

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 let username: string;
+let userChoice: string;
+let comChoice: string;
+let message: string;
 let userScore = 0;
 let comScore = 0;
+let draws = 0;
 
 const btns = document.querySelectorAll(".btn");
 const userInput = document.querySelector(".username") as HTMLInputElement;
@@ -25,3 +29,35 @@ userBtn?.addEventListener("click", () => {
     error!.textContent = "You must enter a username to continue."
   }
 });
+
+// Plays the round after choices are submitted
+const playRound = (playerChoice: string, comChoice: string) => {
+  if (playerChoice === comChoice) {
+    message = `Draw! Both users selected ${playerChoice}.`
+    return draws++;
+  } else if (playerChoice === 'Rock') {
+    if (comChoice === 'Paper') {
+      message = 'The computer wins this round! Paper beats rock.';
+      return comScore++;
+    } else {
+      message = `${username} wins this round! Rock beats scissors.`;
+      return userScore++;
+    }
+  } else if (playerChoice === 'Paper') {
+    if (comChoice === 'Scissors') {
+      message = 'The computer wins this round! Scissors beats paper.';
+      return comScore++;
+    } else {
+      message = `${username} wins this round! Paper beats rock.`;
+      return userScore++;
+    }
+  } else {
+    if (comChoice === 'Rock') {
+      message = 'The computer wins this round! Rock beats scissors.';
+      return comScore++;
+    } else {
+      message = `${username} wins this round! Scissors beats paper.`;
+      return userScore++;
+    };
+  };
+};
