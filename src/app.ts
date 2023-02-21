d021c074f7ae9cdfc735bfa607321a1a9501d6d7
benzelcurry <benawhiting@gmail.com>
2023-02-21 15:11:03 -0800

    Make gameover screen dynamic

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ let draws = 0;
 const btns = document.querySelectorAll('.btn');
 const userInput = document.querySelector('.username') as HTMLInputElement;
 const userBtn = document.querySelector('.submit');
+const playBtn = document.querySelector('.play-btn');
 const userPrompt = document.querySelector('.form-group');
 const error = document.querySelector('.error');
 const game = document.querySelector('.game');
@@ -16,6 +17,7 @@ const messageElement = document.querySelector('.message');
 const playerScore = document.querySelector('.user-score');
 const computerScore = document.querySelector('.com-score');
 const drawScore = document.querySelector('.draws-score');
+const gameover = document.querySelector('.gameover');
 
 btns.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -40,6 +42,14 @@ userBtn?.addEventListener('click', () => {
   }
 });
 
+playBtn?.addEventListener('click', () => {
+  gameover?.classList.remove('visible');
+  game?.classList.add('visible');
+  userScore = 0;
+  comScore = 0;
+  draws = 0;
+})
+
 // Determines computer's choice
 const playCom = () => {
   const choice = Math.floor(Math.random() * 3);
@@ -60,7 +70,9 @@ const determineWinner = (
 ) => {
   winner === username ? userScore++ : comScore++;
   if (userScore === 5 || comScore === 5) {
-    
+    gameover?.classList.add('visible');
+    game?.classList.remove('visible');
+    document.querySelector('.win-msg')!.textContent = `${winner} has won the game!`;
   } else {
     return (message = `${winner} wins this round! ${choiceOne} beats ${choiceTwo}.`);
   }
